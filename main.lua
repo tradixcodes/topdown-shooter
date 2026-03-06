b0f92cb7329843692db392fcef4559bc0dc4cfc8
tradixcodes <tradixiv@gmail.com>
2026-03-06 15:32:14 +0300

    D: Added a few features

--- main.lua
+++ main.lua
@@ -13,31 +13,99 @@ function love.load()
 	player.y = love.graphics.getHeight() / 2
 	player.speed = 120
 
+	myFont = love.graphics.newFont(30)
+
 	zombies = {}
+	bullets = {}
+
+	gameState = 1
+	maxTime = 2
+	timer = maxTime
 end
 
 function love.update(dt)
-	if love.keyboard.isDown("s") then
-		player.y = player.y + player.speed * dt
-	end
-	if love.keyboard.isDown("w") then
-		player.y = player.y - player.speed * dt
-	end
-	if love.keyboard.isDown("a") then
-		player.x = player.x - player.speed * dt
-	end
-	if love.keyboard.isDown("d") then
-		player.x = player.x + player.speed * dt
+	if gameState == 2 then
+		if love.keyboard.isDown("s") then
+			player.y = player.y + player.speed * dt
+		end
+		if love.keyboard.isDown("w") then
+			player.y = player.y - player.speed * dt
+		end
+		if love.keyboard.isDown("a") then
+			player.x = player.x - player.speed * dt
+		end
+		if love.keyboard.isDown("d") then
+			player.x = player.x + player.speed * dt
+		end
 	end
 
 	for i, z in ipairs(zombies) do
 		z.x = z.x + (math.cos(zombiePlayerAngle(z)) * z.speed * dt)
 		z.y = z.y + (math.sin(zombiePlayerAngle(z)) * z.speed * dt)
+
+		if distanceBetween(z.x, z.y, player.x, player.y) < 50 then
+			for i, z in ipairs(zombies) do
+				zombies[i] = nil
+				gameState = 1
+			end
+		end
+	end
+
+	for i, b in ipairs(bullets) do
+		b.x = b.x + (math.cos(b.direction) * b.speed * dt)
+		b.y = b.y + (math.sin(b.direction) * b.speed * dt)
+	end
+
+	for i = #bullets, 1, -1 do
+		local b = bullets[i]
+
+		if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+			table.remove(bullets, i)
+		end
+	end
+
+	for i, z in ipairs(zombies) do
+		for j, b in ipairs(bullets) do
+			if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+				z.dead = true
+				b.dead = true
+			end
+		end
+	end
+
+	for i = #zombies, 1, -1 do
+		local z = zombies[i]
+
+		if z.dead == true then
+			table.remove(zombies, i)
+		end
+	end
+
+	for i = #bullets, 1, -1 do
+		local b = bullets[i]
+
+		if b.dead == true then
+			table.remove(bullets, i)
+		end
+	end
+
+	if gameState == 2 then
+		timer = timer - dt
+		if timer <= 0 then
+			spawnZombie()
+			maxTime = 0.95 * maxTime
+			timer = maxTime
+		end
 	end
 end
 
 function love.draw()
 	love.graphics.draw(sprites.background, 0, 0)
+
+	if gameState == 1 then
+		love.graphics.setFont(myFont)
+		love.graphics.printf("Click anywhere to begin!", 0, 50, love.graphics.getWidth(), "center")
+	end
 
 	love.graphics.draw(
 		sprites.player,
@@ -62,11 +130,30 @@ function love.draw()
 			sprites.zombie:getHeight() / 2
 		)
 	end
+
+	for i, b in ipairs(bullets) do
+		love.graphics.draw(
+			sprites.bullet,
+			b.x,
+			b.y,
+			nil,
+			0.35,
+			nil,
+			sprites.bullet:getWidth() / 2,
+			sprites.bullet:getHeight() / 2
+		)
+	end
 end
 
 function love.keypressed(key)
 	if key == "space" then
 		spawnZombie()
+	end
+end
+
+function love.mousepressed(x, y, button)
+	if button == 1 then
+		spawnBullet()
 	end
 end
 
@@ -80,8 +167,38 @@ end
 
 function spawnZombie()
 	local zombie = {}
-	zombie.x = math.random(zombieWidth, love.graphics.getWidth() - zombieWidth)
-	zombie.y = math.random(zombieHeight, love.graphics.getHeight() - zombieHeight)
-	zombie.speed = 100
+	zombie.x = 0
+	zombie.y = 0
+	zombie.speed = 140
+	zombie.dead = false
+
+	local side = math.random(1, 4)
+	if side == 1 then
+		zombie.x = -30
+		zombie.y = math.random(0, love.graphics.getHeight())
+	elseif side == 2 then
+		zombie.x = love.graphics.getWidth() + 30
+		zombie.y = math.random(0, love.graphics.getHeight())
+	elseif side == 3 then
+		zombie.x = math.random(0, love.graphics.getWidth())
+		zombie.y = -30
+	elseif side == 4 then
+		zombie.x = math.random(0, love.graphics.getWidth())
+		zombie.y = love.graphics.getHeight() + 30
+	end
 	table.insert(zombies, zombie)
+end
+
+function spawnBullet()
+	local bullet = {}
+	bullet.x = player.x
+	bullet.y = player.y
+	bullet.speed = 500
+	bullet.direction = playerMouseAngle()
+	bullet.dead = false
+	table.insert(bullets, bullet)
+end
+
+function distanceBetween(x1, y1, x2, y2)
+	return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 end
